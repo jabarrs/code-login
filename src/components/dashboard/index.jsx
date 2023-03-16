c2233fde91c09e9ee7b0ea3613c6a7775c79379b
jabaww <jabaruddinsyukri@gmail.com>
2023-03-16 14:55:51 +0700

    update dashboard

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -2,39 +2,47 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Dashboard = ({ user }) => {
-  const userLog = user.name;
+  const userLog = user.username;
+  const [search, setSearch] = useState('');
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => setPosts(response.data));
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((response) => setPosts(response.data));
   }, []);
+  const filterProduct = (search) => {
+    const updatedList = posts.filter((post) => post.search === search);
+    // setFilter(updatedList);
+  };
   console.log(userLog);
   console.log(user);
+  console.log(posts);
+
   return (
-    <section className='section'>
-      <label className='title is-3'>Cinta coding</label>
-      <label style={{ marginLeft: '400px' }} className='title-right is-3'>
+    <section className="section">
+      <label className="title is-3">Cinta coding</label>
+      <label style={{ marginLeft: '700px' }} className="title-right is-3">
         Welcome, <b>{userLog}</b>
       </label>
-      <div className='hero'>
-        <div className='container has-text-centered'>
-          <div className='columns is-centered'>
-            <div className='column is-6-desktop'>
-              <div className='panel has-text-centered'>
-                <p className='panel-heading'>Post</p>
-                <div className='panel-block'>
-                  <p className='control has-icons-left'>
-                    <input className='input is-rounded' type='text' placeholder='Search' />
-                    <span className='icon is-left'>
-                      <i className='fas fa-search' aria-hidden='true'></i>
+      <div className="hero">
+        <div className="container has-text-centered">
+          <div className="columns is-centered">
+            <div className="column is-6-desktop">
+              <div className="panel has-text-centered">
+                <p className="panel-heading">Post</p>
+                <div className="panel-block">
+                  <p className="control has-icons-left">
+                    <input className="input is-rounded" type="text" placeholder="Search" />
+                    <span className="icon is-left">
+                      <i className="fas fa-search" aria-hidden="true"></i>
                     </span>
                   </p>
                 </div>
-                <div className='posts-container'>
+                <div className="posts-container">
                   {posts.map((post) => {
                     return (
-                      <div className='post-card' key={post.id}>
-                        <h2 className='post-title'>{post.title}</h2>
-                        <p className='post-body'>{post.body}</p>
+                      <div className="post-card" key={post.id}>
+                        <h2 className="post-title">{post.title}</h2>
+                        <p className="post-body">{post.body}</p>
                       </div>
                     );
                   })}
